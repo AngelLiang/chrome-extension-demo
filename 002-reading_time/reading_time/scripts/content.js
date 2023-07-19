4287c9e96242ca76334d943360b79ea40f43735f
yannanxiu <ynanxiu@qq.com>
2023-07-19 09:28:05 +0800

    doc: 添加注释

--- 002-reading_time/reading_time/scripts/content.js
+++ 002-reading_time/reading_time/scripts/content.js
@@ -2,12 +2,15 @@ const article = document.querySelector("article");
 
 // `document.querySelector` may return null if the selector doesn't match anything.
 if (article) {
+  // 计算阅读时间
   const text = article.textContent;
   const wordMatchRegExp = /[^\s]+/g; // Regular expression
   const words = text.matchAll(wordMatchRegExp);
   // matchAll returns an iterator, convert to array to get word count
   const wordCount = [...words].length;
   const readingTime = Math.round(wordCount / 200);
+
+  // 创建阅读所需时间的元素
   const badge = document.createElement("p");
   // Use the same styling as the publish information in an article's header
   badge.classList.add("color-secondary-text", "type--caption");
